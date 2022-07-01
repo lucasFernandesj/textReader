e3e7183b965774069ca37fae8f5d38fa0c611679
lucasFernandesj <fernandes.lucas.jo@gmail>
2022-07-01 11:04:40 +0300

    no bugs

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,5 @@
+import { pureFinalPropsSelectorFactory } from "react-redux/es/connect/selectorFactory";
+
 let initState={
     text:""
 }
@@ -22,9 +24,11 @@ export const reducer =(state = initState , action )=>{
             if(noDoubleComments.includes("/*") && noDoubleComments.includes("*/")){
                 removeDoubleComments(noDoubleComments)
             }else{
-                noComments = noDoubleComments
                 if(noDoubleComments.includes("//")){
                     removeSingleComment(noDoubleComments)
+                }else{
+                    noComments = noDoubleComments
+
                 }
             }
           
@@ -37,25 +41,37 @@ export const reducer =(state = initState , action )=>{
             }
 
             function removeSingleComment(str){
-                let position1 = str.indexOf("//")
-                let position2 = str.indexOf('\r\n')
-                let comment = str.substr(position1 , position2)
-                let part1=str.substr(0 , position1 )
-                let part2 = str.substr(position2 , str.length-1)
-                let noSingleComment = part1 + part2
-                if(noSingleComment.includes("//")){
-                    removeSingleComment(noSingleComment)
-                }
-                else{
-
-                    noComments = noSingleComment
-                    // console.log(noComments)
-                    if(noComments.includes("/*")&&noComments.includes("*/")){
-                        removeDoubleComments(noComments)
+            //    console.log(str)
+            let commentStart = str.indexOf('//')
+            let lineBreak;   
+            //    console.log("comment starts at "+commentStart)
+               let strArr=str.split('')
+            
+               for(let i = commentStart ; i < strArr.length ; i++){
+                        if(strArr[i]==="\r" && strArr[i+1]==='\n'){
+                            // console.log("line break at "+i)
+                            lineBreak = i
+                            break;
+                        }else{
+                            lineBreak = str.length
+                        }
                     }
-                }
+                    let commentRemoved = str.substr(0 , commentStart)+str.substr(lineBreak , str.length-1)
+                    console.log("comment removed "+commentRemoved)
+                    if(commentRemoved.includes("//")){
+                        removeSingleComment(commentRemoved)
+                    }else if(commentRemoved.includes("/*") && commentRemoved.includes("*/")){
+                        removeDoubleComments(commentRemoved)
+                    }else{
+                        noComments = commentRemoved
+                    }
+
+                    
+
+               
             }
 
+            
 
 
            
